fb87413a6c957be982a4d0aec22e32416ac1be12
xwxxi <1923656649@qq.com>
2023-06-26 23:58:49 +0800

    路由配置、scss自动导入

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/common.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
